fa2774b9f313346b9b9168dba7b20d3c8c6bee6c
Neel Patel <neelcpatel@hotmail.com>
2022-05-12 17:24:58 +0300

    ples work

--- src/perception/src/LaneCentering/laneCenteringNODE.py
+++ src/perception/src/LaneCentering/laneCenteringNODE.py
@@ -590,7 +590,10 @@ def func():
     while(True):
         pubSpeed.publish(0.10)
         #time.sleep(1000)
-        #pub.publish(18.1)
+        pub.publish(18.1)
+        time.sleep(1000)
+        pub.publish(-18.1)
+
 
 if __name__ == '__main__':
 
